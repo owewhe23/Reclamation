345ac8d6f0fd15806bbdf60f316c0e1bac46c1a7
owewhe23 <owewhe23@kingshigh.school.nz>
2021-06-11 12:22:56 +1200

    stuff happened

--- Player.gd
+++ Player.gd
@@ -11,9 +11,9 @@ onready var anim_player = $Graphics/AnimationPlayer
 
 func _physics_process(delta):
 	var move_dir = 0
-	if Input.action_is_pressed("move_right"):
+	if Input.is_action_pressed("move_right"):
 		move_dir += 1
-	if Input.action_is_pressed("move_left"):
+	if Input.is_action_pressed("move_left"):
 		move_dir -= 1
 	
 	move_and_slide(Vector3(move_dir * MOVE_SPEED, y_velo, 0), Vector3(0,1,0))
@@ -29,3 +29,25 @@ func _physics_process(delta):
 		if Input.is_action_pressed("jump"):
 			y_velo = JUMP_FORCE
 			just_jumped = true
+	
+	if move_dir < 0 and facing_right:
+		flip()
+	if move_dir > 0 and !facing_right:
+		flip()
+	
+	if just_jumped:
+		play_anim("Jump - Player")
+	elif grounded:
+		if move_dir == 0:
+			play_anim("Idle - Player")
+		else:
+			play_anim("Run - Player")
+
+func play_anim(anim):
+	if anim_player.current_animation == anim:
+		return
+	anim_player.play()
+
+func flip():
+	$Graphics.rotation_degrees.y *=-1
+	facing_right = !facing_right
