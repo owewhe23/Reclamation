6e4aedf9220efa4c24590ed9ce2c41c6d03c9ada
owewhe23 <owewhe23@kingshigh.school.nz>
2021-06-17 09:57:52 +1200

    thigns wokr

--- Player.gd
+++ Player.gd
@@ -1,21 +1,31 @@
 extends KinematicBody
 
 const MOVE_SPEED = 10
-const JUMP_FORCE = 12
-const GRAVITY = 12
-const MAX_FALL_SPEED = 30
+const JUMP_FORCE = 15
+const GRAVITY = 15
+const MAX_FALL_SPEED = 50
 var y_velo = 0
 var facing_right = false
+var attacking = false
+
+signal attack_over
 
 onready var anim_player = $Graphics/AnimationPlayer
 
 func _physics_process(delta):
 	self.transform.origin.z = 1
 	var move_dir = 0
-	if Input.is_action_pressed("move_right"):
-		move_dir += 1
-	if Input.is_action_pressed("move_left"):
-		move_dir -= 1
+	
+	if attacking == false:
+		if Input.is_action_pressed("move_right"):
+			move_dir += 1
+		if Input.is_action_pressed("move_left"):
+			move_dir -= 1
+	elif attacking == true:
+		pass
+	
+	if Input.is_action_pressed("attack"):
+		attacking = true
 	
 	move_and_slide(Vector3(move_dir * MOVE_SPEED, y_velo, 0), Vector3(0,1,0))
 	
@@ -25,7 +35,7 @@ func _physics_process(delta):
 	if y_velo < -MAX_FALL_SPEED:
 		y_velo = -MAX_FALL_SPEED
 	
-	if grounded:
+	if grounded and attacking == false:
 		y_velo = -0.1
 		if Input.is_action_pressed("jump"):
 			y_velo = JUMP_FORCE
@@ -36,13 +46,17 @@ func _physics_process(delta):
 	if move_dir > 0 and !facing_right:
 		flip()
 	
-	if just_jumped:
-		$Graphics/AnimationPlayer.play("Jump_Player")
-	elif grounded:
-		if move_dir == 0:
-			$Graphics/AnimationPlayer.play("Idle - Player")
-		else:
-			$Graphics/AnimationPlayer.play("Run - Player")
+	if attacking == false:
+		if just_jumped:
+			$Graphics/AnimationPlayer.play("Jump_Player")
+		elif grounded:
+			if move_dir == 0:
+				$Graphics/AnimationPlayer.play("Idle - Player")
+			else:
+				$Graphics/AnimationPlayer.play("Run - Player")
+	elif attacking == true and grounded:
+		$Graphics/AnimationPlayer.play("Attack_Player")
+		
 
 
 func play_anim(anim):
@@ -53,3 +67,7 @@ func play_anim(anim):
 func flip():
 	$Graphics.rotation_degrees.y *=-1
 	facing_right = !facing_right
+
+
+func _on_AnimationPlayer_animation_finished(Attack_Player):
+	attacking = false
