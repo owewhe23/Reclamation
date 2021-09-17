8c888358eb5526fa39ea7c9a28ddf8d7edf24662
owewhe23 <owewhe23@kingshigh.school.nz>
2021-09-17 12:23:59 +1200

    backdroops

--- Backdrop.gd
+++ Backdrop.gd
@@ -8,9 +8,6 @@ var image = Image.new()
 
 
 # Called when the node enters the scene tree for the first time.
+
+
 func _ready():
-	image.load("res://2021 - Reclamation/Backdrop.jpg")
-	texture.create_from_image(image)
-	$Sprite.texture = texture
-
-
